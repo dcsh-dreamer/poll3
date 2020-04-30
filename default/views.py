e34e5c92b3ed53a52e8e2a3619721d5dee5979ff
Dino Wei <dreamer@dcsh.tp.edu.tw>
2020-04-30 12:03:18 +0800

    投票主題列表：苦工版

--- default/views.py
+++ default/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Poll
 
 # Create your views here.
+def poll_list(req):
+    polls = Poll.objects.all()
+    return render(req, 'poll_list.html', {'poll_list': polls})
